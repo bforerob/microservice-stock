a0908cc6ba3c89724cf8b7eb709a285fce731b44
bforerob <bforerob@unal.edu.co>
2024-08-24 19:26:00 -0500

    Create project

---
+++ src/main/java/com/microservice/stock/MicroserviceStockApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/stock/MicroserviceStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
