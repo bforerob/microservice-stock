5405e7d1ae2a40eb958e2ab3424e5382c031e705
bforerob <bforerob@unal.edu.co>
2024-09-05 21:07:13 -0500

    Get Categories eceptions corrections

--- src/main/java/com/microservice/stock/adapters/driven/jpa/mysql/exception/InvalidPageNumberException.java
+++
@@ -1,7 +0,0 @@
-package com.microservice.stock.adapters.driven.jpa.mysql.exception;
-
-public class InvalidPageNumberException extends RuntimeException {
-    public InvalidPageNumberException(String message) {
-        super(message);
-    }
-}
