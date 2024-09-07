7fed54dfc69dd10178dd9e4772a63fd8c89dbd72
bforerob <bforerob@unal.edu.co>
2024-09-07 12:38:06 -0500

    Get paginated brands tests implementation

--- src/test/java/com/microservice/stock/adapters/driving/http/controller/BrandRestControllerAdapterTest.java
+++ src/test/java/com/microservice/stock/adapters/driving/http/controller/BrandRestControllerAdapterTest.java
@@ -6,6 +6,7 @@ import com.microservice.stock.adapters.driving.http.mapper.request.IBrandRequest
 import com.microservice.stock.adapters.driving.http.mapper.response.IBrandResponseMapper;
 import com.microservice.stock.domain.api.IBrandServicePort;
 import com.microservice.stock.domain.model.Brand;
+import com.microservice.stock.domain.util.CustomPage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +15,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.any;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -59,5 +64,43 @@ class BrandRestControllerAdapterTest {
         verify(brandServicePort).addBrand(any(Brand.class));
         verify(brandResponseMapper).toBrandResponse(any(Brand.class));
     }
+
+    @Test
+    @DisplayName("Given correct parameters, should return ok status and custom page with paginated brands")
+    void When_ParametersAreCorrect_Expect_ReturnOkStatus_Brand() throws Exception {
+
+        Brand brand1 = new Brand(1L, "Brand A", "Description A");
+        Brand brand2 = new Brand(2L, "Brand B", "Description B");
+        List<Brand> brands = Arrays.asList(brand1, brand2);
+
+        CustomPage<Brand> brandCustomPage = new CustomPage<>(brands, 0, 2, 2L, 1);
+
+        BrandResponse brandResponse1 = new BrandResponse(1L, "Brand A", "Description A");
+        BrandResponse brandResponse2 = new BrandResponse(2L, "Brand B", "Description B");
+        List<BrandResponse> brandResponses = Arrays.asList(brandResponse1, brandResponse2);
+
+        when(brandServicePort.getAllBrands(anyInt(), anyInt(), anyString(), anyString()))
+                .thenReturn(brandCustomPage);
+        when(brandResponseMapper.toBrandResponsesList(anyList())).thenReturn(brandResponses);
+
+        mockMvc.perform(get("/brand/")
+                        .param("pageNumber", "0")
+                        .param("pageSize", "2")
+                        .param("sortBy", "name")
+                        .param("sortDirection", "asc"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[0].id").value(1L))
+                .andExpect(jsonPath("$.content[0].name").value("Brand A"))
+                .andExpect(jsonPath("$.content[1].id").value(2L))
+                .andExpect(jsonPath("$.content[1].name").value("Brand B"))
+                .andExpect(jsonPath("$.pageNumber").value(0))
+                .andExpect(jsonPath("$.pageSize").value(2))
+                .andExpect(jsonPath("$.totalElements").value(2L))
+                .andExpect(jsonPath("$.totalPages").value(1));
+
+        verify(brandServicePort).getAllBrands(anyInt(), anyInt(), anyString(), anyString());
+        verify(brandResponseMapper).toBrandResponsesList(anyList());
+    }
+
 
 }
--- src/test/java/com/microservice/stock/domain/api/usecase/BrandUseCaseTest.java
+++ src/test/java/com/microservice/stock/domain/api/usecase/BrandUseCaseTest.java
@@ -1,10 +1,9 @@
 package com.microservice.stock.domain.api.usecase;
 
-import com.microservice.stock.domain.exception.AlreadyExistsException;
-import com.microservice.stock.domain.exception.EmptyFieldException;
-import com.microservice.stock.domain.exception.LengthFieldException;
+import com.microservice.stock.domain.exception.*;
 import com.microservice.stock.domain.model.Brand;
 import com.microservice.stock.domain.spi.IBrandPersistencePort;
+import com.microservice.stock.domain.util.CustomPage;
 import com.microservice.stock.domain.util.DomainConstants;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,8 +12,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class BrandUseCaseTest {
@@ -91,5 +92,61 @@ class BrandUseCaseTest {
                 , "addBrand did not throw the length field exception");
 
     }
+
+    @Test
+    @DisplayName("Should return paginated brands list when all inputs are valid")
+    void When_getAllBrandsValidInput_Expect_ReturnsPaginatedBrands() {
+
+        Integer pageNumber = 0;
+        Integer pageSize = 10;
+        String sortBy = "name";
+        String sortDirection = "ASC";
+
+        List<Brand> brands = List.of(new Brand(1L, "Nike", "Sports brand"));
+        CustomPage<Brand> expectedPage = new CustomPage<>(brands, pageNumber, pageSize, 1L, 1);
+
+        when(brandPersistencePort.getAllBrands(pageNumber, pageSize, sortBy, sortDirection))
+                .thenReturn(expectedPage);
+
+        CustomPage<Brand> result = brandUseCase.getAllBrands(pageNumber, pageSize, sortBy, sortDirection);
+
+        assertEquals(expectedPage, result, "getAllBrands did not return expected page brands");
+        verify(brandPersistencePort, times(1)).getAllBrands(pageNumber, pageSize, sortBy, sortDirection);
+    }
+
+    @Test
+    @DisplayName("Should throw InvalidSortDirectionException when sortDirection is invalid")
+    void _Expect_InvalidSortDirectionException_SortDirectionIsInvalid_Brand() {
+
+        Integer pageNumber = 0;
+        Integer pageSize = 10;
+        String sortBy = "name";
+        String invalidSortDirection = "invalid";
+
+        InvalidSortDirectionException exception = assertThrows(InvalidSortDirectionException.class, () ->
+                brandUseCase.getAllBrands(pageNumber, pageSize, sortBy, invalidSortDirection)
+        );
+
+        assertEquals(invalidSortDirection, exception.getMessage(), "getAllBrands did not throw the expected invalid sort direction exception");
+        verify(brandPersistencePort, never()).getAllBrands(anyInt(), anyInt(), anyString(), anyString());
+    }
+
+    @Test
+    @DisplayName("Should throw InvalidSortParameterException when sort parameter is invalid")
+    void Expect_ThrowsInvalidSortParameterException_When_SortParameterIsInvalid_Brand() {
+
+        Integer pageNumber = 0;
+        Integer pageSize = 10;
+        String invalidSortBy = "invalid";
+        String sortDirection = "ASC";
+
+        InvalidSortParameterException exception = assertThrows(InvalidSortParameterException.class, () ->
+                brandUseCase.getAllBrands(pageNumber, pageSize, invalidSortBy, sortDirection)
+        );
+
+        assertEquals(invalidSortBy, exception.getMessage(), "getAllBrands did not throw the expected invalid sort parameter exception");
+        verify(brandPersistencePort, never()).getAllBrands(anyInt(), anyInt(), anyString(), anyString());
+    }
+
 
 }
